b7aff7bfec7b52246a72bbaabd9a3d131ee2c545
herobrin234444 <93288140+herobrin234444@users.noreply.github.com>
2022-08-17 16:05:49 -0300

    Add files via upload

--- App.js
+++ App.js
@@ -1,10 +1,35 @@
 import React,{Component} from "react";
+import { Rajdhani_600SemiBold } from "@expo-google-fonts/rajdhani";
+import * as Font from "expo-font";
+
 import BottomTabNavigator from "./components/BottomTabNavigator";
 
 export default class App extends Component{
- render(){
-  return(
-    <BottomTabNavigator/>
-  )
- } 
+  constructor(){
+    super();
+    this.state = {
+      fontLoad:false
+    }
+  }
+
+  async LoadFonts(){
+     await Font.loadAsync({
+      Rajdhani_600SemiBold:Rajdhani_600SemiBold
+     });
+     this.setState({
+      fontLoad:true
+     })
+  }
+
+  componentDidMount(){
+    this.LoadFonts();
+  }
+
+ render(){ 
+  const {fontLoad} = this.state;
+  if (fontLoad === true){
+  return <BottomTabNavigator/>
+ }
+ return null; 
+}
 }
--- components/BottomTabNavigator.js
+++ components/BottomTabNavigator.js
@@ -1,7 +1,8 @@
 import React,{Component}  from "react"
 import {NavigationContainer} from "@react-navigation/native";
 import {createBottomTabNavigator} from "@react-navigation/bottom-tabs";
-import {SafeAreaProvider} from "react-native-safe-area-context";
+import Ionicons from "react-native-vector-icons/Ionicons"
+
 
 import SearchScreen from "../screens/Search";
 import TransactionScreen from "../screens/transaction";
@@ -10,14 +11,55 @@ const Tab = createBottomTabNavigator();
 export default class BottomTabNavigator extends Component{
     render(){
         return(
-            <SafeAreaProvider>
-                <NavigationContainer>
+                <NavigationContainer
+                screenOptions ={({route}) => ({
+                    tabBarIcon:({focused,size,color})=> {
+                        let iconName;
+                        if(route.name === "Transação"){
+                            iconName = "book";
+                        }else if(route.name === "Pesquisa"){
+                            iconName ="search"
+                        }
+
+                        return(
+                            <Ionicons
+                            name={iconName}
+                            size={size}
+                            color={color}
+                         />
+                        );
+
+                    }
+                })}
+
+                tabBarOptions = {{
+                    activeTintColor: "#FFFFFF",
+                     inactiveTintColor: "black",
+                      style: {
+                         height: 130, 
+                         borderTopWidth: 0, 
+                         backgroundColor: "#5653d4" },
+                       labelStyle: { 
+                        fontSize: 20, 
+                        fontFamily: "Rajdhani_600SemiBold" },
+                        labelPosition: "beside-icon",
+                         tabStyle: { 
+                            marginTop: 25, 
+                            marginLeft: 10, 
+                            marginRight: 10, 
+                            borderRadius: 30, 
+                            borderWidth: 2, 
+                            alignItems: "center", 
+                            justifyContent: "center", 
+                            backgroundColor: "#5653d4" }
+                }}
+            
+                >
                     <Tab.Navigator>
                         <Tab.Screen name="Transação" component ={TransactionScreen} />
                         <Tab.Screen name="Pesquisa" component ={SearchScreen} />
                     </Tab.Navigator>
                 </NavigationContainer>
-            </SafeAreaProvider>
         )
     }
 }
